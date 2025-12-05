81acbb358233f7e898e950b0331bab2d0a5082c2
Gayathri-12s <gowri7350@gmail.com>
2025-12-05 11:22:20 +0530

    Add Bootstrap and jQuery for styling and interactivity; replace App.js with App.jsx for updated component structure

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css';
+import images from "./images/f1.jpg";
+
+console.log("React app started");
+
+function App() {
+
+   
+    let  userName = "Gayathri";
+   
+    return (
+
+      <div className='container d-flex justify-content center mt-5 '>
+          
+     
+     <div className = "card" >
+      <h2> Welcome to React Learning, {userName} </h2>
+     
+
+     <img  src = {images} className = "img-fluid mx-auto" alt = "Responsive Image" width = "200" height = "200"/>
+     <div className = "card-body mb-3">
+      <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
+    </div>
+     <img src ="https://m.media-amazon.com/images/I/71cL+bBACrL._SX679_.jpg" className = "img-fluid mx-auto" alt = "Responsive Image" width = "200" height = "200"/>
+   
+    <div className = "card-body">
+      <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
+    </div>
+  
+    </div>
+</div>
+  
+    
+
+     
+
+
+    );
+ 
+  
+  
+ 
+}
+
+export default App;
