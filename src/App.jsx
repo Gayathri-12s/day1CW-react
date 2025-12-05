e53b47d6f2b376e3f8b0c4e7140f5a483fc040df
Gayathri-12s <gowri7350@gmail.com>
2025-12-05 11:50:36 +0530

    Refactor App component layout and styling; simplify CSS and enhance card structure

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,28 @@ function App() {
    
     return (
 
-      <div className='container d-flex justify-content center mt-5 '>
+
+      <div className="container d-flex justify-content-center mt-5 vh-100 " >
           
      
-     <div className = "card" >
-      <h2 className='text-center 'style={{color:"orange", fontFamily: "sans-serif"}}> Welcome to React Learning, {userName} </h2>
+         <div className = "card" style={{width: "500px"}} >
+            <h2 className='text-center 'style={{color:"orange", fontFamily: "sans-serif"}}> Welcome to React Learning, {userName} </h2>
      
 
-     <img  src = {images} className = "img-fluid mx-auto mt-3" alt = "Responsive Image" width = "200" height = "200"/>
-     <div className = "card-body mb-3">
-      <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
-    </div>
-     <img src ="https://m.media-amazon.com/images/I/71cL+bBACrL._SX679_.jpg" className = "img-fluid mx-auto" alt = "Responsive Image" width = "200" height = "200"/>
+            <img  src = {images} className = "img-fluid mx-auto mt-3" alt = "Responsive Image" width = "200" height = "200"/>
+            <div className = "card-body mb-3">
+            <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
+          </div>
+            <img src ="https://m.media-amazon.com/images/I/71cL+bBACrL._SX679_.jpg" className = "img-fluid mx-auto" alt = "Responsive Image" width = "200" height = "200"/>
    
-    <div className = "card-body">
-      <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
-    </div>
+         <div className = "card-body">
+         <p className = "card-text text-center text-muted">This is your first card with images and styles! </p>
+        </div>
   
     </div>
 </div>
   
+
     
 
      
